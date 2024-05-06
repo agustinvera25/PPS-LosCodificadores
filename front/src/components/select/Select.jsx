285edce28529d7308d9e4178430da959b25ca8e4
Giuliana <giuliana9566@gmail.com>
2024-05-06 20:23:57 -0300

    Creamos componente Select

---
+++ front/src/components/select/Select.jsx
@@ -0,0 +1,15 @@
+import './Select.css';
+export const Select =() => {
+    return (
+      <select className="btn" >
+    <option value="cabaña">Cabaña</option>
+    <option value="hosteria">Hosteria</option>
+    <option value="apart">Apart</option>
+    <option value="camping">Camping</option>
+
+    </select>
+    );
+  };
+    
+
+
